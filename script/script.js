07ea47d9e44824f634e3693ea30a8eb653bfb15b
TaRek-ElSayed02 <Tarekswe24@gmail.com>
2025-02-05 23:51:32 +0200

    add text on image and do animation to text to change at time

--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
 let list = document.querySelectorAll('.unordered li');
 
+list.forEach(item => {
+    item.addEventListener('click', () => {
+        list.forEach(li => {
+            li.classList.remove('active');
+        });
+        item.classList.add('active');
+    });
+});
